989f2762c00c2856a0e5b0bb47d6bb6767476649
Muhammad462 <139973126+Muhammad462@users.noreply.github.com>
2023-08-02 20:34:39 +0500

    first commit

---
+++ index.js
@@ -0,0 +1,8 @@
+"use strict";
+//task 21: intentional error
+const contries = ["Pakistan", "America", "England", "China", "Palastine"];
+{
+    console.log("The list of contries");
+    console.log(contries[5]); //this will cause an index error
+    console.log(contries[4]); //correct codes
+}
---
+++ index.ts
@@ -0,0 +1,7 @@
+//task 21: intentional error
+const contries = ["Pakistan","America","England","China","Palastine"]
+{
+    console.log("The list of contries")
+    console.log(contries[5])//this will cause an index error
+    console.log(contries[4])//correct codes
+}
